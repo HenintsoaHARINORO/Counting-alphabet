1bc51c328879428df48c5ba0f05ed0d79f0726da
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-06-15 13:14:39 +0300

    entering the alphabet

--- src/Count.java
+++ src/Count.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Count {
      public static void main(String [] args)
      {
-    	 System.out.println("Count the numbers of alphabet in a sentence");
+    	 System.out.println("*****Count the numbers of alphabet in a sentence*****");
     	 System.out.print("Enter your  sentence:");
     	 Scanner sc=new Scanner(System.in);
     	 String str=sc.nextLine();
-    	 System.out.print("Your  sentence is:"+str);
+    	 System.out.println("Your  sentence is:"+str);
+    	 System.out.println("Please enter the alphabet you want to count:");
+    	 String alpha=sc.nextLine();
      }
 }
