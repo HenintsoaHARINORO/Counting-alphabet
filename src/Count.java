07b67c1105bb8e8e37baf7b8babbfd0448e130bb
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-06-15 13:07:41 +0300

    receiving from input

---
+++ src/Count.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Count {
+     public static void main(String [] args)
+     {
+    	 System.out.println("Count the numbers of alphabet in a sentence");
+    	 System.out.print("Enter your  sentence:");
+    	 Scanner sc=new Scanner(System.in);
+    	 String str=sc.nextLine();
+    	 System.out.print("Your  sentence is:"+str);
+     }
+}
